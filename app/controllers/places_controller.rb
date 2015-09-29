3e9b69684a82b7aef98e57a59b71d48caecd32f0
Kimberly Yi <yikim01@gmail.com>
2015-09-29 13:49:33 -0700

    models and controllers set

---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,30 @@
+class PlacesController < ApplicationController
+  def index
+    @places = Place.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
